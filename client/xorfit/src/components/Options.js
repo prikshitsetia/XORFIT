1398c1d921c94988b9d4a1ce16b60746ea5b475d
Apoorva Kadam <pragatikadam008@gmail.com>
2020-12-16 00:11:58 +0530

    Added new field in Option form

--- client/xorfit/src/components/Options.js
+++ client/xorfit/src/components/Options.js
@@ -7,7 +7,9 @@ export default class Options extends Component {
     
         this.state = {
              time:'',
-             option:'meditation'
+             option:'meditation',
+             showMedOptions:true,
+             medOption:'Mindfulness'
         }
     }
     
@@ -19,10 +21,24 @@ export default class Options extends Component {
        
     }
 
+    handelChange = e =>{
+        this.setState({option:e.target.value})
+        if (this.state.option == "yoga"){
+            this.setState({
+                showMedOptions:true
+            })
+        }else{
+            this.setState({
+                showMedOptions:false
+            })
+
+        }
+    }
+
     render() {
         return (
             <div>
-                <br/><br/>
+                <br/>
                 <div className="row">
                     
 
@@ -32,7 +48,6 @@ export default class Options extends Component {
 
                     
                     <div className="col-md-6" >
-                        <br/>
 
                         <div className="container" style={{width:"550px",background:"black",borderRadius:"8px",marginRight:"280px",marginTop:"70px"}}>
 
@@ -55,7 +70,7 @@ export default class Options extends Component {
 
                                     <label style={{float:"left"}}>Which activity would you preffer?</label>
                                     <select className="form-control" value={this.state.option} 
-                                    onChange={(e)=>{this.setState({option:e.target.value})}}>
+                                    onChange={this.handelChange}>
 
                                     <option value="meditation">Meditation</option>
                                     <option value="yoga">Yoga</option>
@@ -64,6 +79,26 @@ export default class Options extends Component {
 
 
                                 </div>
+
+                                {   this.state.showMedOptions && (
+
+                                    <div className="form-group m-1">
+
+                                    <label style={{float:"left"}}>What do yu want to work on?</label>
+                                    <select className="form-control" value={this.state.medOption} 
+                                    onChange={(e)=>{this.setState({medOption:e.target.value})}}>
+
+                                    <option value="meditation">Mindfulness</option>
+                                    <option value="yoga">Realeasing Stress</option>
+                                    <option value="yoga">Realeasing Anxiety</option>
+                                        
+                                    </select> 
+
+
+                                    </div>
+
+                                )}
+                                
 
                                 <br/>
                                 <button className="btn btn-primary  btn-block m-2" type="submit" style={{border:"none",color:"white",background:"#a30a3a"}}>
