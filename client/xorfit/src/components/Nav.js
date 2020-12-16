49beccbc16e7c252182b0c89be647dd0e9a4952a
Prikshit <prikshitsetia1878@gmail.com>
2020-12-17 00:57:05 +0530

    Added music player for meditation

--- client/xorfit/src/components/Nav.js
+++ client/xorfit/src/components/Nav.js
@@ -1,47 +1,24 @@
-import React, { Component } from 'react'
-import '../general.css'
-import { Link , BrowserRouter as Router} from 'react-router-dom'
-
+import React, { Component } from "react";
+import "../general.css";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 export default class Nav extends Component {
-   
-    render() {
-               return (
+  render() {
+    return (
+      <div className="container-fluid" style={{ background: "black" }}>
+        <Router>
+          <nav className="navbar navbar-expand-lg navbar-dark">
+            <ul className="navbar-nav ml-auto">
+              <Link to="/options">
+                <li className="nav-item">Home</li>
+              </Link>
 
-                
-
-                <div className="container-fluid" style={{background:"black"}}>
-                    <Router>
-                        
-                        <nav className="navbar navbar-expand-lg navbar-dark">
-                            <ul className="navbar-nav ml-auto">
-
-                            <Link to="/options">
-                                <li className="nav-item">
-                                    Home 
-                                </li>
-                                
-                            </Link>
-                           
-                            <li className="nav-item">
-                                Dashboard 
-                            </li>
-                            <li className="nav-item">
-                                Logout 
-                            </li>
-
-                           
-
-                            </ul>
-
-
-                        </nav>
-
-                                            
-                    </Router>
-                
-                
-            </div>
-                    )
-    }
+              <li className="nav-item">Dashboard</li>
+              <li className="nav-item">Logout</li>
+            </ul>
+          </nav>
+        </Router>
+      </div>
+    );
+  }
 }
